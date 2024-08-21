8523f88a80419c71c01aa36e94fe8fe90615e1b7
hhdyhaha <1164549585@qq.com>
2024-08-21 22:23:01 +0800

    需求demo初始化

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import NodeDemo from './components/NodeDemo.vue'
+</script>
+
+<template>
+  <NodeDemo />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/NodeDemo.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    节点需求初始化
+  </div>
+</template>
+
+<style scoped>
+</style>
