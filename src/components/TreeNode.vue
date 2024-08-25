76d0a354ad57be07e9e9f6389f12b000a1b44126
hhdyhaha <1164549585@qq.com>
2024-08-25 21:00:57 +0800

    fix(TreeNode): 修复多于一个子节点时的UI显示问题在TreeNode组件中，当一个节点有多于一个子节点时，界面显示不正确。此次修复包括两部分内容： 1. 调整CommonCard组件的样式，正确显示多子节点的连接线。 2. 在TreeNode组件中添加计算父子节点间宽度的逻辑，以动态调整连接线的长度。
    此更改确保在节点有多于一个子节点时，UI能正确显示连接线，提升用户体验。

--- src/components/TreeNode.vue
+++ src/components/TreeNode.vue
@@ -2,6 +2,7 @@
 import CommonCard from "@/components/CommonCard.vue";
 import { useNodesStore } from "@/stores/index.ts";
 import { storeToRefs } from 'pinia'
+import { ref, onMounted,  onUnmounted} from 'vue'
 const store = useNodesStore()
 const { multiNodes } = storeToRefs(store)
 
@@ -12,17 +13,49 @@ const props = defineProps({
   }
 });
 
-if(props.node.children && props.node.children.length > 1){
-  multiNodes.value.push({'node_id':props.node.node_id})
+const nodeRef = ref(null)
+
+if (props.node.children && props.node.children.length > 1) {
+  multiNodes.value.push({ 'node_id': props.node.node_id })
 }
+
+function findParentNode() {
+  if (props.node.children && props.node.children.length > 1) {
+    if (nodeRef.value) {
+      console.log(nodeRef.value.offsetWidth)
+      const parentLineAfter = document.createElement('div')
+      parentLineAfter.className = 'parent-line-after'
+      parentLineAfter.style.width = `${nodeRef.value.offsetWidth-250}px`
+      parentLineAfter.style.height = `10px`
+      parentLineAfter.style.backgroundColor = '#D8D8D8'
+      parentLineAfter.style.position = 'absolute'
+      parentLineAfter.style.left = `${120}px`
+      nodeRef.value.appendChild(parentLineAfter)
+    }
+  }
+}
+
+onMounted(() => {
+  findParentNode()
+  window.addEventListener('resize', findParentNode);
+  window.addEventListener('scroll', findParentNode);
+})
+
+onUnmounted(() => {
+  window.removeEventListener('resize', findParentNode);
+  window.removeEventListener('scroll', findParentNode);
+});
+
+// 滚动监听
+
 </script>
 
 <template>
   <div class="tree-node">
-    <CommonCard :node="node"/>
-<!--    <div v-if="node.children && node.children.length > 1" class="parent-line"></div>-->
-    <div v-if="node.children && node.children.length" class="children-box">
-      <TreeNode v-for="child in node.children" :key="child.node_id" :node="child"/>
+    <CommonCard :node="node" />
+    <div v-if="node.children && node.children.length > 1" class="parent-line"></div>
+    <div v-if="node.children && node.children.length" class="children-box" ref="nodeRef">
+      <TreeNode v-for="child in node.children" :key="child.node_id" :node="child" />
     </div>
   </div>
 </template>
@@ -41,13 +74,13 @@ if(props.node.children && props.node.children.length > 1){
   display: flex;
   justify-content: flex-start;
   flex-wrap: nowrap;
+  position: relative; // Ensure parent-line-after is positioned relative to this container
 }
 
 .parent-line {
   position: relative;
   width: 10px;
   height: 50px;
-  margin-top: -10px;
   background-color: #D8D8D8;
 }
 </style>
